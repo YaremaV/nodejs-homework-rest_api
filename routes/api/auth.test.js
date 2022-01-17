9c419376f9bec635bb51c4d137d9c6b61f941516
YaremaV <Yaremenko1996v@gmail.com>
2022-01-17 20:25:47 +0200

    Add test

--- routes/api/auth.test.js
+++ routes/api/auth.test.js
@@ -1,13 +1,27 @@
 const mongoose = require("mongoose");
 const request = require("supertest");
+
 const app = require("../../app");
+const { User } = require("../../model/user");
+
+require("dotenv").config();
+
+const { DB_TEST_HOST } = process.env;
 
 describe("test auth", () => {
   let server;
   beforeAll(() => (server = app.listen(3000)));
   afterAll(() => server.close());
 
-  beforeeach(() => {});
+  beforeEach((done) => {
+    mongoose.connect(DB_TEST_HOST).then(() => done());
+  });
+
+  afterEach((done) => {
+    mongoose.connection.db.dropDatabase(() => {
+      mongoose.connection.close(() => done());
+    });
+  });
 
   test("test register route", async () => {
     const registerUser = {
@@ -18,5 +32,13 @@ describe("test auth", () => {
     const response = await request(app)
       .post("./api/auth/signup")
       .send(registerUser);
+
+    expect(response.statusCode).toBe(201);
+    expect(response.body.message).toBe("Register success");
+
+    const user = await User.findByIdAndUpdate(response.body._id);
+    expect(user).toByThruthy();
+    expect(user.name).toBe(registerUser.name);
+    expect(user.email).toBe(registerUser.email);
   });
 });
