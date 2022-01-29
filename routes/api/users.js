d79f3377399edb4cd559f9ae5a20de5ea9dc6b97
YaremaV <Yaremenko1996v@gmail.com>
2022-01-29 22:10:07 +0200

    create verify to users

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-
+const { NotFound, BadRequest } = require("http-errors");
 const { User } = require("../../model");
 const { authenticate, upload } = require("../../middlewares");
 
+const { sendEmail } = require("../../helpers");
 const router = express.Router();
-
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
+
+const { SITE_NAME } = process.env;
 
 router.get("/logout", authenticate, async (req, res, next) => {
   const { _id } = req.user;
@@ -40,5 +42,57 @@ router.patch(
     res.json({ avatarURL });
   }
 );
+
+router.get("/verify/:verificationToken", async (req, res, next) => {
+  try {
+    const { verificationToken } = req.params;
+    const user = await User.findOne({ verificationToken });
+
+    if (!user) {
+      throw new NotFound("User not found");
+    }
+
+    await User.findByIdAndUpdate(user._id, {
+      verificationToken: null,
+      verify: true,
+    });
+
+    res.json({ message: "Verification successful" });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/verify", async (req, res, next) => {
+  try {
+    const { email } = req.body;
+
+    if (!email) {
+      throw new BadRequest("missing required field email");
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      throw new NotFound("User not found");
+    }
+
+    if (user.verify) {
+      throw new BadRequest("Verification has already been passed");
+    }
+
+    const { verificationToken } = user;
+    const data = {
+      to: email,
+      subject: "Подтверждение email",
+      html: `<a target= "_blank" href ="https://${SITE_NAME}/users/verify/${verificationToken}">Подтвердите email</a>`,
+    };
+    await sendEmail(data);
+
+    res.json({ message: "Verification email sent" });
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
